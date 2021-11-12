46c1a97b5bac82d19ee998940f172d440f063444
justengel <jtengel08@gmail.com>
2021-11-11 21:17:23 -0500

    Removed field_property from this library may eventually push pydantic_property library.

--- pydantic_decoder/__init__.py
+++ pydantic_decoder/__init__.py
@@ -1,4 +1,4 @@
 from .__meta__ import version as __version__
 
 from .decoder import DecoderModelMetaclass, DecoderModel, encode, decode,\
-    get_model_name, get_model, register_model, remove_model  # 'field_property',
+    get_model_name, get_model, register_model, remove_model
